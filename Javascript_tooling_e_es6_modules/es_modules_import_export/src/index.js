082997c3322df273a2e99fc6edffa8c9446fc3de
Nipodemos <nipodemos13@gmail.com>
2020-05-10 00:43:43 -0300

    Seção 9: Javascript Tooling e ES6 modules: ES6 Modules - Import/Export

---
+++ Javascript_tooling_e_es6_modules/es_modules_import_export/src/index.js
@@ -0,0 +1,7 @@
+import multiplica, { nome, sobrenome, idade, soma } from './modulo1'
+
+console.log('nome :>> ', nome);
+console.log('sobrenome :>> ', sobrenome);
+console.log('idade :>> ', idade);
+console.log('soma() :>> ', soma(5, 5));
+console.log('multiplica(3,9) :>> ', multiplica(3, 9));
---
+++ Javascript_tooling_e_es6_modules/es_modules_import_export/src/modulo1.js
@@ -0,0 +1,11 @@
+export const nome = "luiz"
+export const sobrenome = "miranda"
+export const idade = 30;
+
+export function soma(x, y) {
+  return x + y;
+}
+
+export default (x,y) => {
+  return x*y
+}

fab4286aff41c3700e9b6ea9a9810d0bfa9283f0
nipodemos13@gmail.com <Nipodemos>
2020-05-17 03:08:09 -0300

    Seção 11: Projeto Agenda - Criando Usuários

--- projeto_agenda/src/controllers/loginController.js
+++ projeto_agenda/src/controllers/loginController.js
@@ -1,7 +1,35 @@
-function index(req, res) {
+const Login = require('../models/LoginModel')
+
+
+exports.index = (req, res) => {
   res.render('login');
 }
 
-module.exports = {
-  index
+exports.register = async (req, res) => {
+
+  try {
+    const login = new Login(req.body)
+    await login.register();
+
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors)
+      req.session.save(function () {
+        return res.redirect('back')
+      })
+    } else {
+      req.flash('success', 'Seu usuário foi criado com sucesso')
+      req.session.save(function () {
+        return res.redirect('back')
+      })
+    }
+    return
+  } catch (error) {
+    console.log('error :>> ', error);
+    return res.render('4040')
+  }
+
+}
+
+exports.login = (req, res) => {
+
 }
---
+++ projeto_agenda/src/models/LoginModel.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+
+const LoginSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+
+});
+
+const LoginModel = mongoose.model('Login', LoginSchema);
+
+class Login {
+  constructor(body) {
+    this.body = body
+    this.errors = [];
+    this.user = null;
+  }
+
+  async register() {
+    this.validarCampos();
+    if (this.errors.length > 0) {
+      return
+    } else {
+      try {
+        this.user = await LoginModel.create(this.body)
+      } catch (error) {
+        console.log('error :>> ', error);
+      }
+    }
+  }
+
+  validarCampos() {
+    this.cleanUp()
+
+    // Validação
+
+    // O email precisa ser válido
+    if (!validator.isEmail(this.body.email)) {
+      this.errors.push('E-mail Inválido')
+    }
+
+    // A senha precisa ter entre 3 e 50 caracteres
+    if (this.body.password.length < 3 || this.body.password.length > 50) {
+      this.errors.push('A senha precisa ter entre 3 e 50 caracteres')
+    }
+  }
+
+  cleanUp() {
+    for (const key in this.body) {
+      if (this.body.hasOwnProperty(key)) {
+        if (typeof this.body[key] !== 'string') {
+          this.body[key] = ''
+        }
+      }
+    }
+
+    this.body = {
+      email: this.body.email,
+      password: this.body.password
+    }
+  }
+}
+
+module.exports = Login;

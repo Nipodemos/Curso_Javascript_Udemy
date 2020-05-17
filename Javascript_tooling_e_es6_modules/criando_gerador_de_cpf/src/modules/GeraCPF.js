30af5262116648f125f110e6e912ff79ee2ae02c
nipodemos13@gmail.com <Nipodemos>
2020-05-17 03:27:09 -0300

    remove espaço em branco aleatório no arquivo GeraCPF

--- Javascript_tooling_e_es6_modules/criando_gerador_de_cpf/src/modules/GeraCPF.js
+++ Javascript_tooling_e_es6_modules/criando_gerador_de_cpf/src/modules/GeraCPF.js
@@ -1,22 +1,5 @@
 import ValidaCPF from './ValidaCPF';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 export default class GeraCPF {
   rand(min = 100000000, max = 999999999) {
     return String(Math.floor(Math.random() * (max - min) + min));
